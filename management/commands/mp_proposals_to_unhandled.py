777f4383ede595b910fb5b314bfad9b7083412ee
Robin Harms Oredsson <robin@betahaus.net>
2025-10-17 14:36:51 +0200

    Script to set proposals as undhandled based on buttons

---
+++ management/commands/mp_proposals_to_unhandled.py
@@ -0,0 +1,132 @@
+from auditlog.context import set_actor
+from django.contrib.contenttypes.models import ContentType
+from django.core.management import BaseCommand
+from django.db import models
+from django.db import transaction
+
+from voteit.agenda.workflows import AgendaItemWf
+from voteit.meeting.models import Meeting
+from voteit.proposal.models import Proposal
+
+from voteit.proposal.workflows import ProposalWf
+from voteit.reactions.models import Reaction
+from voteit.reactions.models import ReactionButton
+
+
+class Command(BaseCommand):
+    help = "Dra tillbaka förslag som inte uppnått speciell gräns"
+    AI_STATES = (AgendaItemWf.ONGOING, AgendaItemWf.UPCOMING)
+    PROP_STATES = (ProposalWf.UNHANDLED,)
+
+    def add_arguments(self, parser):
+        parser.add_argument("-m", help="Meeting pk", required=True)
+        parser.add_argument(
+            "-p", help="PK för återyrka-knapp med target", required=True
+        )
+        parser.add_argument(
+            "-i",
+            help="Ingorera förslag med följande knappar aktiva",
+            action="extend",
+            nargs="+",
+            type=int,
+        )
+        parser.add_argument(
+            "-b",
+            help="Välj bara förslag med följande knappar aktiva",
+            required=True,
+            action="extend",
+            nargs="+",
+            type=int,
+        )
+        parser.add_argument(
+            "-u",
+            help="PK för användare som utför operationen - måste vara del av mötet",
+            required=True,
+        )
+        parser.add_argument(
+            "--commit",
+            help="Commit result to db",
+            action="store_true",
+            default=False,
+        )
+
+    def handle(self, *args, **options):
+        meeting: Meeting = Meeting.objects.get(pk=options.get("m"))
+        commit = options.get("commit")
+        republish_target_btn: ReactionButton = meeting.reaction_buttons.get(
+            pk=options.get("p")
+        )
+        if not republish_target_btn.target:
+            exit("Återpublicera-knappen måste ha target satt.")
+        btns = meeting.reaction_buttons.filter(pk__in=options["b"], flag_mode=True)
+        if btns.count() != len(options["p"]):
+            exit("Knapp-IDn stämmer inte med mötesknappar i flagg-läge.")
+        if options["i"]:
+            ignore_btns = meeting.reaction_buttons.filter(
+                pk__in=options["i"], flag_mode=True
+            )
+            if ignore_btns.count() != len(options["i"]):
+                exit(
+                    "Knapp-IDn för ignorera stämmer inte med mötesknappar i flagg-läge."
+                )
+        else:
+            ignore_btns = []
+        user = meeting.participants.get(pk=options.get("u"))
+
+        ai_qs = meeting.agenda_items.filter(state__in=self.AI_STATES)
+        if ai_count := ai_qs.count():
+            self.stdout.write(f"Processing {ai_count} agenda items")
+        else:
+            self.stdout.write(self.style.WARNING("No matching agenda items"))
+            exit("Nothing to do")
+
+        prop_qs = Proposal.objects.filter(
+            agenda_item__in=ai_qs, state__in=ProposalWf.PUBLISHED
+        )
+        # Find proposals that match specific flags
+        relevant_proposal_ids = Reaction.objects.filter(
+            button__in=btns,
+            object_id__in=prop_qs.values("pk"),
+            content_type=ContentType.objects.get_for_model(Proposal),
+        ).values_list("object_id", flat=True)
+        # Find reactions over target
+        self.stdout.write(
+            f"Target for {republish_target_btn} is {republish_target_btn.target}"
+        )
+        over_target_proposal_ids = (
+            republish_target_btn.reactions.filter(
+                object_id__in=prop_qs.values("pk"),
+                content_type=ContentType.objects.get_for_model(Proposal),
+            )
+            .values("object_id")
+            .order_by()
+            .annotate(count=models.Count("pk"))
+            .filter(count__gte=republish_target_btn.target)
+        )
+        self.stdout.write(
+            f"Found {over_target_proposal_ids.count()} proposals over target."
+        )
+        relevant_proposal_ids -= over_target_proposal_ids
+        if ignore_btns:
+            relevant_proposal_ids -= Reaction.objects.filter(
+                button__in=ignore_btns,
+                object_id__in=prop_qs.values("pk"),
+                content_type=ContentType.objects.get_for_model(Proposal),
+            ).values_list("object_id", flat=True)
+
+        prop_qs = Proposal.objects.filter(
+            pk__in=relevant_proposal_ids, agenda_item__meeting=meeting
+        )
+        with transaction.atomic(durable=True):
+            with set_actor(user):
+                for prop in prop_qs:
+                    prop.unhandled()
+                    prop.save()
+
+            if commit:
+                self.stdout.write(self.style.SUCCESS("All done, saving"))
+            else:
+                self.stdout.write(
+                    self.style.WARNING("DRY-RUN: Specify --commit to save")
+                )
+                transaction.set_rollback(True)
